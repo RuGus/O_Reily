7a09359cfae0fedb42919caddf7d620b682933ce
RuGus <35694691+RuGus@users.noreply.github.com>
2018-07-18 12:57:05 +0300

    Add files via upload

---
+++ IfTest.java
@@ -0,0 +1,10 @@
+class IfTest{
+	public static void main (String[] args){
+	int x = 3;
+	if (x==3) {
+		System.out.println ("X must be "+x);
+	}
+	System.out.println("Xa-xa-xa");
+	//System.out.println("Add new row");
+	}
+}
